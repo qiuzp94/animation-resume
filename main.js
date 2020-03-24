a1f44f57863640f0d2184dae66fb555cfe3eb9d0
qiuzp <qiuzp94@foxmail.com>
2020-03-24 23:40:58 +0800

    新增异步&回调

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+function writeCode(prefix, code, fn) {
+  let domCode = document.querySelector('#code')
+  let n = 0
+  let id = setInterval(() => {
+    n += 1
+    domCode.innerHTML =
+      Prism.highlight(prefix + code.substring(0, n), Prism.languages.css, 'css');
+    styleTag.innerHTML = prefix + code.substring(0, n)
+    domCode.scrollTop = 10000
+    if (n >= code.length) {
+      window.clearInterval(id)
+      fn.call() 
+    }
+  }, 10)
+}
+
 var result = `/*
 * 面试官您好，我叫Qiuzp
 * 下面请允许我用动画的形式来介绍一下我自己
@@ -8,28 +24,67 @@ var result = `/*
 
 *{
 transition:all 1s;
-margin:0;
-padding:0;
-box-sizing:border-box;
 }
 
 body{
 background:#C7FFF9;
+font-size:18px;
 color:#000;
-border:3px solid #45BEB1;
+border:3px solid #333;
 padding:20px;
+}
+
+/* 我需要一点代码高亮 */
+.token.selector{
+  color:#690;
+}
+.token.property{
+  color:#905;
+}
+.token.punctuation{
+  color:#999;
+} 
+
+/* 加点3D效果 */
+#code{
+  transform:rotate(360deg);
+}
+
+/* 下面正式介绍一下我自己吧*/
+/* 通灵之术-白纸召唤之术*/ 
+`
+
+var result2 = `
+#paper{
+  width:100px; height:100px;
+  background:white;
 }
 `
 
-var n = 0
-var id = setInterval(()=>{
-  n += 1 
-  code.innerHTML = result.substring(0,n)
-  code.innerHTML = 
-    Prism.highlight(code.innerHTML, Prism.languages.css,'css');
-  styleTag.innerHTML = result.substring(0,n)
-  if(n >= result.length){
-      window.clearInterval(id)
-  }  
-},10)
+writeCode('', result, () => {
+  createPaper(() => {
+    writeCode(result, result2)
+  })
+})
 
+function createPaper(fn) {
+  var paper = document.createElement('pre')
+  paper.id = 'paper'
+  document.body.appendChild(paper)
+  fn.call()
+}
+
+function fn3(preResult) {
+  var n = 0
+  var id = setInterval(() => {
+    n += 1
+    code.innerHTML = preResult + result.substring(0, 1)
+    code.innerHTML =
+      Prism.highlight(code.innerHTML, Prism.languages.css, 'css');
+    styleTag.innerHTML = preResult + result.substring(0, n)
+    if (n >= result.length) {
+      window.clearInterval(id)
+    }
+  }, 10)
+
+}
